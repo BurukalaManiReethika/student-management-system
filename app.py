e4167cedc591433609333c6e79e13f220034c852
BURUKALA MANI REETHIKA <manireethikab@gmail.com>
2026-06-20 10:49:04 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,60 @@
+from flask import Flask, render_template, request, redirect, url_for
+import sqlite3
+
+app = Flask(__name__)
+DB = "students.db"
+
+def init_db():
+    conn = sqlite3.connect(DB)
+    cur = conn.cursor()
+
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
+        age INTEGER,
+        course TEXT
+    )
+    """)
+
+    conn.commit()
+    conn.close()
+
+@app.route("/")
+def index():
+    conn = sqlite3.connect(DB)
+    students = conn.execute("SELECT * FROM students").fetchall()
+    conn.close()
+    return render_template("index.html", students=students)
+
+@app.route("/add", methods=["GET", "POST"])
+def add_student():
+    if request.method == "POST":
+        name = request.form["name"]
+        age = request.form["age"]
+        course = request.form["course"]
+
+        conn = sqlite3.connect(DB)
+        conn.execute(
+            "INSERT INTO students(name, age, course) VALUES (?, ?, ?)",
+            (name, age, course)
+        )
+        conn.commit()
+        conn.close()
+
+        return redirect(url_for("index"))
+
+    return render_template("add.html")
+
+@app.route("/delete/<int:id>")
+def delete_student(id):
+    conn = sqlite3.connect(DB)
+    conn.execute("DELETE FROM students WHERE id=?", (id,))
+    conn.commit()
+    conn.close()
+
+    return redirect(url_for("index"))
+
+if __name__ == "__main__":
+    init_db()
+    app.run(debug=True)
